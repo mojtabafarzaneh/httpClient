4f6fc922b410d8a913fed9fd911b424b05792ac4
Mojtaba <mojtabafarzaneh78@gmail.com>
2025-08-15 12:05:04 +0330

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mojtabafarzaneh/httpClinet
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal("Error opening file:", err)
+	}
+	defer f.Close()
+
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
+}
