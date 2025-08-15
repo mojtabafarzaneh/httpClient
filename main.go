904d89f1383498794a04d8bbb55615d71f986ee3
Mojtaba <mojtabafarzaneh78@gmail.com>
2025-08-15 14:14:19 +0330

    thirdRound

--- main.go
+++ main.go
@@ -3,36 +3,52 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal("Error opening file:", err)
 	}
-	str := ""
 
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-
-			fmt.Printf("read: %s\n", str)
-			str = ""
-
-		}
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Println("line:", line)
 	}
 }
